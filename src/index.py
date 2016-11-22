89b5f966be9ffe502673de3241def7525f5f8aba
Jacint Virag <viragjacint@gmail.com>
2016-11-22 11:06:16 +0000

    Finalised html pages, created db tables and added content from db to pages.

--- src/index.py
+++ src/index.py
@@ -21,7 +21,7 @@ def init(app):
 		app.config['url'] = config.get("config", "url")
 		app.config['username'] = config.get("config", "username")
 		app.config['password'] = config.get("config", "password")
-		app.config['db_location'] = 'db/music.db'
+		app.config['db_location'] = 'db/shop.db'
 		app.secret_key  = config.get("config", "secret_key")
 		
 		app.config['log_file'] = config.get("logging", "name")
@@ -46,9 +46,63 @@ def logs(app):
 def root():
 	this_route = url_for('.root')
 	app.logger.info("Logging a test message from "+this_route)
-	return render_template('layout.html')	
+	sql = ('SELECT * FROM product_list WHERE featured = 1')
+	connection = sqlite3.connect(app.config['db_location'])
+	connection.row_factory = sqlite3.Row     		
+	rows = connection.cursor().execute(sql).fetchall()	
+	connection.close()
+	return render_template('home.html', rows = rows)	
 
-			
+@app.route('/register')
+def register():	
+	return render_template('register.html')
+
+@app.route('/cart')
+def cart():	
+	return render_template('cart.html')	
+
+@app.route('/product')
+def product():
+	sql = ('SELECT * FROM product_list')
+	connection = sqlite3.connect(app.config['db_location'])
+	connection.row_factory = sqlite3.Row     		
+	rows = connection.cursor().execute(sql).fetchall()
+	connection.close()	
+	return render_template('product.html', rows = rows)
+	
+@app.route('/details', methods=['GET'])
+@app.route('/details/<id>', methods=['GET'])
+def details(id):
+	sql = ('SELECT * FROM product_list WHERE id = ?')
+	connection = sqlite3.connect(app.config['db_location'])
+	connection.row_factory = sqlite3.Row     		
+	rows = connection.cursor().execute(sql, [id]).fetchall()
+	connection.close()	
+	return render_template('details.html', rows = rows)
+	
+@app.route('/login')
+def login():	
+	return render_template('login.html')	
+
+@app.route('/sign_in', methods=['GET', 'POST'])
+def sign_in():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['username']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['password']:
+			error = 'Invalid password'
+		else:
+			session['user'] = True
+			session['username'] = 'Jacint'
+			return redirect(url_for('root'))
+	return render_template('login.html', error=error)
+
+@app.route('/sign_out')
+def sign_out():
+    session.pop('user', None)    
+    return redirect(url_for('root'))
+	
 if __name__ == '__main__':
 	init(app)
 	logs(app)
